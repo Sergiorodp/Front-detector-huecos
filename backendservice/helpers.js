c90e468f3371294c40650ad458a8c01d98fd45b2
Sergiorodp <sergio.rod.perez@outlook.es>
2022-06-02 18:37:14 -0500

    agregado acrtualizacaion bakService

--- backendservice/helpers.js
+++ backendservice/helpers.js
@@ -1,8 +1,8 @@
-// const baseUrl = 'https://detector-huecos.herokuapp.com/DetectorHuecos/v1'
-// const basePORT = 'https://detector-huecos.herokuapp.com'
+const baseUrl = 'https://detector-huecos.herokuapp.com/DetectorHuecos/v1'
+const basePORT = 'https://detector-huecos.herokuapp.com'
 
-const baseUrl = 'http://localhost:3001/DetectorHuecos/v1'
+// const baseUrl = 'http://localhost:3001/DetectorHuecos/v1'
 // const basePORT = 'http://127.0.0.1/'
-const basePORT = 'http://localhost:3001'
+// const basePORT = 'http://localhost:3001'
 
 export default { baseUrl, basePORT }
